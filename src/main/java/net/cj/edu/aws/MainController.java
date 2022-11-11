adf8b8f9fd3cf01de4ba5e87d0a2095f1a96db65
jcwooo <jcwooo@host.docker.internal>
2022-11-11 14:42:26 +0900

    메인페이지

--- src/main/java/net/cj/edu/aws/MainController.java
+++ src/main/java/net/cj/edu/aws/MainController.java
@@ -1,5 +1,8 @@
 package net.cj.edu.aws;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class MainController {
 	
@@ -7,5 +10,4 @@ public class MainController {
 	public String home() {
 		return "<h1>aws-devops</h1>";
 	}
-
 }
